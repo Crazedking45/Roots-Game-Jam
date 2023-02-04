b099ac5ff783a2c7436275f134600b3d47026670
Smile <smailbestybay@gmail.com>
2023-02-04 12:47:26 -0800

    added acceleration

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,21 +6,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private float speed = 1.5f;
+    private float speed;
 
     [SerializeField] private float speedRotate = 50.0f;
+
+    // acceleration variable
+    public float accelerationTime = 60; 
+    float maxSpeed = 10f;
+    private float minSpeed = 1.5f;
+    private float time;
+
     void Start()
     {
         transform.position = new Vector3(0, 0);
+        minSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // acceleration
+        speed = Mathf.SmoothStep(minSpeed, maxSpeed, time / accelerationTime);
+
         transform.position += speed * Time.deltaTime * -transform.right;
 
         var axis = Input.GetAxis("Horizontal");
         
         transform.Rotate(axis * speedRotate * Time.deltaTime * Vector3.forward);
+        time += Time.deltaTime;
     }
 }
